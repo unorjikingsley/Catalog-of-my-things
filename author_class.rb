848d216003d976c25c34fc1250950189a09525f6
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-08-08 10:53:26 +0100

    Add author class and its list handler

---
+++ author_class.rb
@@ -0,0 +1,14 @@
+class Author
+  attr_reader :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+end
