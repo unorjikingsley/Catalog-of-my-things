296ab9156ff1abcc48255cce8517e1f17ae69b77
paul-tes <pauasca8462@gmail.com>
2023-08-10 06:04:01 +0300

    Modify items name

--- Student-A/database/schema.sql
+++ Student-A/database/schema.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE catalog_of_my_things;
 
 -- Create the items table
-CREATE TABLE items (
+CREATE TABLE item (
   id SERIAL PRIMARY KEY,
   genre_id integer,
   author_id integer,
@@ -30,12 +30,12 @@ CREATE TABLE labels (
 );
 
 -- Add foreign key relationships using ALTER TABLE
-ALTER TABLE items ADD FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE;
-ALTER TABLE items ADD FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE;
-ALTER TABLE items ADD FOREIGN KEY (source_id) REFERENCES sources(id) ON DELETE CASCADE;
-ALTER TABLE items ADD FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE;
+ALTER TABLE item ADD FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE;
+ALTER TABLE item ADD FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE;
+ALTER TABLE item ADD FOREIGN KEY (source_id) REFERENCES sources(id) ON DELETE CASCADE;
+ALTER TABLE item ADD FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE;
 
 ALTER TABLE books ADD FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE;
-ALTER TABLE books ADD FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE;
+ALTER TABLE books ADD FOREIGN KEY (item_id) REFERENCES item(id) ON DELETE CASCADE;
 
 ALTER TABLE labels ADD FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE;
