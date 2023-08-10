5aa60168c54b8185bb1b9194f7596c1ab0a6a763
paul-tes <pauasca8462@gmail.com>
2023-08-10 05:59:24 +0300

    Add relationships

--- Student-A/database/schema.sql
+++ Student-A/database/schema.sql
@@ -29,3 +29,13 @@ CREATE TABLE labels (
   book_id integer
 );
 
+-- Add foreign key relationships using ALTER TABLE
+ALTER TABLE items ADD FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE;
+ALTER TABLE items ADD FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE;
+ALTER TABLE items ADD FOREIGN KEY (source_id) REFERENCES sources(id) ON DELETE CASCADE;
+ALTER TABLE items ADD FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE;
+
+ALTER TABLE books ADD FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE;
+ALTER TABLE books ADD FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE;
+
+ALTER TABLE labels ADD FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE;
