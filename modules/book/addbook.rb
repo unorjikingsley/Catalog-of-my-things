fcf166ad5ef81765a311e5fac1f0dae5403204d1
paul-tes <pauasca8462@gmail.com>
2023-08-11 09:44:56 +0300

    Add book Fixed

--- modules/book/addbook.rb
+++ modules/book/addbook.rb
@@ -1,15 +1,40 @@
-class Label
-  attr_accessor :items, :color, :title, :id
-
-  def initialize(color, title, id = nil)
-    @id = id || Random.rand(1..1000)
-    @title = title
-    @color = color
-    @items = []
+module AddBook
+  def add_book
+    publish_date = input_date
+    publisher = input_publisher
+    cover_state = input_cover_state
+    book = Book.new(cover_state, publisher, publish_date)
+    @books << book
+    label = add_label(book)
+    book.add_label(label)
+    puts 'Book album Added Successfully'
+    puts 'Press Enter to continue!'
+    gets.chomp
   end
 
-  def add_item(item)
-    @items << item
-    item.label = self
+  def input_date
+    puts 'Please Enter publish date in following format: yyyy/mm/dd'
+    gets.chomp
+  end
+
+  def input_publisher
+    puts 'Enter Publisher'
+    gets.chomp
+  end
+
+  def input_cover_state
+    puts 'Enter Cover state'
+    gets.chomp
+  end
+
+  def add_label(book)
+    puts 'Add a label'
+    puts 'Enter Label Color'
+    color = gets.chomp
+    puts 'Enter Label title'
+    title = gets.chomp
+    label = Label.new(color, title, book.id)
+    @label << label
+    label
   end
 end
